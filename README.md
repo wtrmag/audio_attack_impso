f99f1bf303cb1ccfe9122e466514b4d013cb78ce
wtrmag <792034552@qq.com>
2021-11-16 22:41:02 +0800

    wait for nn

---
+++ README.md
@@ -0,0 +1,20 @@
+First clone the DeepSpeech repository and download the model at the appropriate version:
+```
+git clone -b v0.1.1 --single-branch --depth 1 https://github.com/mozilla/DeepSpeech.git
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.1.1/deepspeech-0.1.1-models.tar.gz
+tar -xzf deepspeech-0.1.1-models.tar.gz && rm deepspeech-0.1.1-models.tar.gz
+```
+Then, create the checkpoint used for the attack:
+```
+python make_checkpoint.py
+```
+DeepSpeech may throw a warning saying "decoder library file does not exist" but that can be ignored.
+
+## Running Attacks
+Now create and run an attack, for example:
+```bash
+python run_audio_attack.py sample_input.wav "hello world"
+``` 
+Of course, `sample_input.wav` may be changed to any input audio file and `"hello world"` may be changed to any target transcription.
+
+You can also listen to pre-created audio samples in the [samples](samples/) directory. Each original/adversarial pair is denoted by a leading number, with model transcriptions as the title.
--- attack.py
+++ attack.py
@@ -1,9 +1,9 @@
 import sys
+import argparse
+import numpy as np
 import tensorflow as tf
 import scipy.io.wavfile as wav
-import numpy as np
 from tensorflow.python.keras.backend import ctc_label_dense_to_sparse
-
 from tf_logits import get_logits
 
 sys.path.append('DeepSpeech')
@@ -29,39 +29,22 @@ def load_wav(input_wav_file):
 
 
 class Attack():
-    def __init__(self, input_wav_file, output_wav_file, phrase, batch_size=1):
+    def __init__(self, input_wav_file, output_wav_file, phrase, batch_size=1, iterations=3000):
         self.audio = load_wav(input_wav_file)
         self.input_audio = np.tile(self.audio, (batch_size, 1))
         self.output_audio = output_wav_file
         self.target_phrase = phrase
         self.batch_size = batch_size
-        self.audio_length = np.tile(len(self.input_audio), batch_size)
+        self.audio_length = np.tile(len(self.audio), batch_size)
         self.target_index = np.tile(np.array([[tokens.index(x) for x in phrase]]), (batch_size, 1))
         self.target_length = np.tile(len(phrase), batch_size)
         self.weird = np.tile((self.audio.shape[0] - 1) // 320, 1)
 
-        # self.input = tf.placeholder(tf.float32, shape=(batch_size, None), name='input')
-        # self.input_length = tf.placeholder(tf.int32, shape=(None,), name='length')
-        # self.target_phrase = tf.placeholder(tf.string, name='phrase')
-        # self.target_in = tf.placeholder(tf.int32, shape=(batch_size, None), name='index')
-        # self.target_length = tf.placeholder(tf.int32, shape=(None,), name='len')
-        # self.arg = tf.placeholder(tf.float32, shape=(None,), name='arg')
+        self.iterations = iterations
 
-        # self.input = tf.Variable(np.zeros((batch_size, audio_length)), dtype=tf.float32)
-        # self.input_length = tf.Variable(np.zeros((batch_size, )), dtype=tf.int32)
-        # self.target_in = tf.Variable(np.zeros((batch_size, target_length)), dtype=tf.int32)
-        # self.target_length = tf.Variable(np.zeros(((batch_size, ))), dtype=tf.int32)
-        # self.arg = tf.Variable(np.zeros((batch_size, )), dtype=tf.int32)
-
-        # self.pass_in = tf.clip_by_value(self.input, -2 ** 15, 2 ** 15 - 1)
-        # self.logits = get_logits(self.pass_in, self.arg)
-        # self.target = ctc_label_dense_to_sparse(self.target_in, self.target_length)
-        # self.ctc_loss = tf.nn.ctc_loss(labels=tf.cast(self.target, tf.int32), inputs=self.logits, sequence_length=self.arg)
-
-    def run_attack(self, sess):
+    def get_loss(self, sess):
         input = tf.placeholder(tf.float32, shape=self.input_audio.shape, name='input')
         input_len = tf.placeholder(tf.int32, shape=self.audio_length.shape, name='length')
-        target_phrase = tf.placeholder(tf.string, name='phrase')
         target_in = tf.placeholder(tf.int32, shape=self.target_index.shape, name='index')
         target_len = tf.placeholder(tf.int32, shape=self.target_length.shape, name='len')
         arg = tf.placeholder(tf.int32, shape=self.weird.shape, name='arg')
@@ -77,38 +60,35 @@ class Attack():
         saver = tf.train.Saver(tf.global_variables())
         saver.restore(sess, "models/session_dump")
 
-        print(sess.run(ctc_loss, feed_dict={input: self.input_audio, input_len: self.audio_length,
-                                                   target_phrase: self.target_phrase, target_in: self.target_index,
-                                                   target_len: self.target_length, arg: self.weird}))
-
-        # print(sess.run(self.ctc_loss, feedict={self.input: input_audio, self.input_length: input_length,
-        #                                        self.target_phrase: phrase, self.target_in: target_in,
-        #                                        self.target_length: target_length, self.arg: weird}))
-
-        # sess.run(tf.global_variables_initializer())
-        # sess.run(tf.assign(self.input, input_audio))
-        # sess.run(tf.assign(self.input_length, input_length))
-        # sess.run(tf.assign(self.target_in, target_in))
-        # sess.run(tf.assign(self.target_length, target_length))
-        # sess.run(tf.assign(self.arg, weird))
-
-        # print(sess.run(self.ctc_loss))
+        ctc_loss, decode = sess.run([ctc_loss, decoded], feed_dict={input: self.input_audio, input_len: self.audio_length, target_in:
+            self.target_index, target_len: self.target_length, arg: self.weird})
+        print('ctc loss :{}'.format(ctc_loss))
+        new_text = ''.join([tokens[i] for i in decode[0].values])
+        print(new_text)
 
 
 if __name__ == '__main__':
-    input_wav_file = sys.argv[1]
-    phrase = sys.argv[2].lower()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, dest='input_wav_file', required=True)
+    parser.add_argument('-t', '--target', type=str, dest='phrase', required=True)
+
+    args = parser.parse_args()
+    while len(sys.argv) > 1:
+        sys.argv.pop()
+
+    input_wav_file = args.input_wav_file
+    phrase = args.phrase.lower()
     out_wav_file = input_wav_file[:-4] + '_adv.wav'
     log_file = input_wav_file[:-4] + '_log.txt'
 
     print('target phrase:', phrase)
     print('source file:', input_wav_file)
 
-    attack = Attack(input_wav_file, out_wav_file, phrase, 1)
+    attack = Attack(input_wav_file, out_wav_file, phrase)
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
 
-        attack.run_attack(sess)
-        # print('the test accuracy :{}'.format())
+        attack.get_loss(sess)
+
 
 
